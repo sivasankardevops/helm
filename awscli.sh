ba5231708ba5d0d14b466b2bd4922ef8d20bde53
sivasankardevops <42856513+sivasankardevops@users.noreply.github.com>
2023-06-16 21:05:27 +0530

    Update awscli.sh

--- awscli.sh
+++ awscli.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 aws --version
+aws configure set aws_access_key_id "AKIA6HGSOCO3EEAH7TFY" --profile siva && aws configure set aws_secret_access_key "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" --profile siva && aws configure set region "us-east-1" --profile siva && aws configure set output "json" --profile siva
+aws eks update-kubeconfig --name helm-practice
+kubectl get nodes
